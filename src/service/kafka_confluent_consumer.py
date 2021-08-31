d358f89bdd46441c2b6cdcb245c4ab397df132cf
leorfk <leonardorodrigues_f@hotmail.com>
2021-08-30 23:37:20 -0300

    adiçao de multiprocessamento no consumer

--- src/service/kafka_confluent_consumer.py
+++ src/service/kafka_confluent_consumer.py
@@ -1,22 +1,17 @@
 from confluent_kafka import Consumer, KafkaException
 from confluent_kafka.admin import AdminClient
 import logging
+from multiprocessing import Process
 from util import logging_config
-
 logging_config.init_logs(logging.INFO)
-kafka_conf = {
-    'bootstrap.servers': '127.0.0.1:9092',
-    'group.id': 'mygroup',
-    'auto.offset.reset': 'earliest',
-    'enable.auto.commit': False
-}
-
-c = Consumer(kafka_conf)
-
-c.subscribe(['xap'])
-
 
 def list_topics():
+    kafka_conf = {
+        'bootstrap.servers': '127.0.0.1:9092',
+        'group.id': 'mygroup',
+        'auto.offset.reset': 'latest',
+        'enable.auto.commit': False
+    }
     admin = AdminClient(kafka_conf)
     topics = admin.list_topics().topics
     logging.info(topics)
@@ -24,52 +19,64 @@ def list_topics():
         logging.info(f'{k} - {len(v.partitions)}')
 
 
-def consume():
+def consume(topic):
+    kafka_conf = {
+        'bootstrap.servers': '127.0.0.1:9092',
+        'group.id': 'mygroup',
+        'auto.offset.reset': 'latest',
+        'enable.auto.commit': False
+    }
+    c = Consumer(kafka_conf)
+    c.subscribe([topic])
     logging.info('Começou!!!')
     contador = 0
     while True:
-        msg = c.poll(1.0)
+        msg = c.poll(timeout=1)
 
         if msg is None:
             contador += 1
-            logging.warning(contador)
+            logging.warning(f'Tópico {topic} passou {contador} vezes sem mensagem')
             if contador == 10:
                 logging.warning('COnsumer será desligado, pois não existem mais eventos a serem consumidos')
-                c.close()
+                continue
             else:
                 continue
         if msg.error():
             logging.error("Consumer error: {}".format(msg.error()))
             continue
-        logging.info(f'''
-        header: {msg.headers()},
-        payload: {msg.value().decode('utf-8')}''')
+        logging.info(f'''header: {msg.headers()},payload: {msg.value().decode('utf-8')}''')
+        c.commit()
 
 
-def toma():
-    msg = None
-    count = 0
-    while msg is None:
-        msg = c.poll(1000)
-        logging.info(msg.value())
-        logging.warning(type(msg))
-
-def eventos_na_estica():
-    logging.info('Começou!!!')
-    file = open('xap.json', 'x')
+def eventos_na_estica(topic):
+    kafka_conf = {
+        'bootstrap.servers': '127.0.0.1:9092',
+        'group.id': 'mygroup',
+        'auto.offset.reset': 'latest',
+        'enable.auto.commit': False
+    }
+    c = Consumer(kafka_conf)
+    c.subscribe([topic])
+    logging.info(f'Começou o xap no tópico {topic}')
     while True:
-            logging.info('tô no aguardo')
-            msgs = c.consume(50000)
-            if not msgs:
-                continue
-            try:
-                for msg in msgs:
-                    # logging.info(f'''header: {msg.headers()},payload: {msg.value().decode('utf-8')}''')
-                    file.write(f'''header: {msg.headers()},payload: {msg.value().decode('utf-8')}''' + '\n')
-                    c.commit(msg)
+        logging.info(f'tô no aguardo {topic}')
+        msgs = c.consume(5000)
+        if not msgs:
+            logging.info('Sem nada aqui')
+            continue
+        try:
+            for msg in msgs:
+                logging.info(f'''header: {msg.headers()},payload: {msg.value().decode('utf-8')}''')
+                c.commit(msg)
 
-            except KafkaException as ex:
-                logging.error(ex)
+        except KafkaException as ex:
+            logging.error(ex)
 
 
-eventos_na_estica()
+if __name__ == '__main__':
+    topics = ['LOJA_NOVO_PEDIDO', 'topico.comando.teste', 'ECOMMERCE_SEND_EMAIL', 'xap', 'ECOMMERCE_NEW_ORDER',
+         'kafka-python-topic']
+    for topic in topics:
+        logging.info(topic)
+        Process(target=eventos_na_estica, args=(topic,)).start()
+        logging.info(f'passamo o famoso: {topic}')
--- src/service/kafka_confluent_producer.py
+++ src/service/kafka_confluent_producer.py
@@ -2,7 +2,8 @@
 from confluent_kafka import Producer
 import logging
 from repository.gerador_de_eventos import generate_event
-
+from util import logging_config
+logging_config.init_logs(logging.INFO)
 p = Producer({'bootstrap.servers': '127.0.0.1:9092'})
 
 
@@ -13,14 +14,18 @@ def delivery_report(err, msg):
         logging.info('Message delivered to {} [{}]'.format(msg.topic(), msg.partition()))
 
 
-def gerar_massa():
-    for c in range(500000):
-        p.poll(0)
-        p.produce(topic='xap',
-                  value=str(generate_event()).encode('utf-8'),
-                  callback=delivery_report,
-                  headers={"toma": "xap"})
+def gerar_massa(topics):
+    for topic in topics:
+        for c in range(300000):
+            p.poll(0)
+            header = {'toma': topic}
+            p.produce(topic=topic,
+                      value=str(generate_event()).encode('utf-8'),
+                      callback=delivery_report,
+                      headers=header)
     p.flush()
 
 
-gerar_massa()
+if __name__ == '__main__':
+    t = ['LOJA_NOVO_PEDIDO','topico.comando.teste','ECOMMERCE_SEND_EMAIL','xap','ECOMMERCE_NEW_ORDER','kafka-python-topic']
+    gerar_massa(t)
